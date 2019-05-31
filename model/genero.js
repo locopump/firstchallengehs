e66aaad53eeaca6903e31c8e234dbcef3bf11432
Frank Cáceres <frank.caceres@latinfintech.com>
2019-05-31 17:24:26 -0500

    ordenamiento de clases

---
+++ model/genero.js
@@ -0,0 +1,6 @@
+class Genero {
+    constructor(id, nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+}
---
+++ model/pais.js
@@ -0,0 +1,6 @@
+class Pais {
+    constructor (id, nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+}
